e1fe850e58af2d2c6b25b252bc55ed83f3a61757
Jens N <jens.n@godlike.biz>
2021-08-16 13:22:34 +0200

    Version 1.1.0

---
+++ DruidMacroHelper.lua
@@ -0,0 +1,152 @@
+local DRUID_MACRO_HELPER_NAME_START = "dmhStart";
+local DRUID_MACRO_HELPER_NAME_RESET = "dmhReset";
+local DRUID_MACRO_HELPER_NAME_END = "dmhEnd";
+local DRUID_MACRO_HELPER_NAME_POT = "dmhPot";
+local DRUID_MACRO_HELPER_NAME_HS = "dmhHs";
+local DRUID_MACRO_HELPER_NAME_SAPPER = "dmhSap";
+local DRUID_MACRO_HELPER_NAME_SUPER_SAPPER = "dmhSuperSap";
+local DRUID_MACRO_HELPER_ITEM_SHORTCUTS = {
+  ["pot"] = 13446,
+  ["potion"] = 13446,
+  ["hs"] = 20520,
+  ["rune"] = 20520,
+  ["seed"] = 20520,
+  ["sapper"] = 10646,
+  ["supersapper"] = 23827,
+  ["drums"] = 13180,
+  ["holywater"] = 13180
+};
+SLASH_DRUID_MACRO_HELPER1 = "/dmh";
+SLASH_DRUID_MACRO_HELPER2 = "/druidmacro";
+
+function DruidShifter(spellId, ...)
+  local preventShift = GetSpellCooldown(spellId)+C_LossOfControl.GetActiveLossOfControlDataCount();
+  local manaCost = 580;
+  local manaCostTable = GetSpellPowerCost(spellId);
+  if (manaCostTable) then
+    for i in ipairs(manaCostTable) do
+      if (manaCostTable[i].type == 0) then
+        manaCost = manaCostTable[i].cost;
+      end
+    end
+  end
+  local itemIds = {...};
+  for i in ipairs(itemIds) do
+    preventShift = preventShift + GetItemCooldown(itemIds[i]);
+  end
+  return (preventShift > 0) or (UnitPower("player",0)<manaCost);
+end
+
+function DruidEnergy(spellId, maxEnergy)
+  local preventShift = GetSpellCooldown(spellId)+C_LossOfControl.GetActiveLossOfControlDataCount();
+  local manaCost = 580;
+  local manaCostTable = GetSpellPowerCost(spellId);
+  if (manaCostTable) then
+    for i in ipairs(manaCostTable) do
+      if (manaCostTable[i].type == 0) then
+        manaCost = manaCostTable[i].cost;
+      end
+    end
+  end
+  if (not maxEnergy) then
+    maxEnergy = 30;
+  end
+  return (preventShift > 0) or (UnitPower("player",0)<manaCost) or (UnitPower("player",3)>maxEnergy);
+end
+
+local function DruidItemIds(itemNamesOrIds)
+  local itemIds = {};
+  for i in ipairs(itemNamesOrIds) do
+    local itemName = strlower(itemNamesOrIds[i]);
+    if DRUID_MACRO_HELPER_ITEM_SHORTCUTS[itemName] then
+      tinsert(itemIds, DRUID_MACRO_HELPER_ITEM_SHORTCUTS[itemName]);
+    else
+      tinsert(itemIds, itemNamesOrIds[i]);
+    end
+  end
+  return itemIds;
+end
+
+local function DruidMacroSlash(action, ...)
+  local params = {...};
+  if (action == "") or (action == "help") then
+    print("|cffff0000DruidMacroHelper|r Available commands:");
+    print("|cffffff00/dmh start|r Disable autoUnshift if player is stunned, on gcd or out of mana");
+    print("|cffffff00/dmh end|r Enable autoUnshift again");
+    print("|cffffff00/dmh stun|r Disable autoUnshift if stunned");
+    print("|cffffff00/dmh cd <itemId|itemShortcut>[ <itemId|itemShortcut> ...]|r Disable autoUnshift if items are on cooldown, player is stunned, on gcd or out of mana");
+    print("|cffffff00/dmh energy <maxEnergy>|r Disable autoUnshift if above given energy value, player is stunned, on gcd or out of mana");
+    print("|cffffff00/click "..DRUID_MACRO_HELPER_NAME_START.."|r Change actionbar based on the current form. (includes /dmh start)");
+    print("|cffffff00/click "..DRUID_MACRO_HELPER_NAME_RESET.."|r Change actionbar back to 1.");
+    print("|cffffff00/click "..DRUID_MACRO_HELPER_NAME_END.."|r Change back to form based on the current bar. (includes /dmh end)");
+    print("|cffffff00/click "..DRUID_MACRO_HELPER_NAME_POT.."|r Disable autoUnshift if not ready to use a potion");
+    print("|cffffff00/click "..DRUID_MACRO_HELPER_NAME_HS.."|r Disable autoUnshift if not ready to use a healthstone");
+    print("|cffffff00/click "..DRUID_MACRO_HELPER_NAME_SAPPER.."|r Disable autoUnshift if not ready to use a sapper");
+    print("|cffffff00/click "..DRUID_MACRO_HELPER_NAME_SUPER_SAPPER.."|r Disable autoUnshift if not ready to use a super sapper");
+  elseif (action == "stun") then
+    -- Disable autoUnshift if stunned
+    if C_LossOfControl.GetActiveLossOfControlDataCount()>0 then
+      SetCVar("autoUnshift", 0);
+    end
+  elseif (action == "cd") then
+    -- Disable autoUnshift if items are on cooldown, player is stunned, on gcd or out of mana
+    local itemIds = DruidItemIds(params);
+    if DruidShifter(768, unpack(itemIds)) then
+      SetCVar("autoUnshift", 0);
+    end
+  elseif (action == "energy") then
+    -- Disable autoUnshift if above given energy value, player is stunned, on gcd or out of mana
+    local energyMax = tonumber(params[1]);
+    if DruidEnergy(768, energyMax) then
+      SetCVar("autoUnshift", 0);
+    end
+  elseif (action == "start") then
+    -- Disable autoUnshift if player is stunned, on gcd or out of mana
+    if DruidShifter(768) then
+      SetCVar("autoUnshift", 0);
+    end
+  elseif (action == "end") then
+    -- Enable autoUnshift again
+    SetCVar("autoUnshift", 1);
+  end
+end
+
+SlashCmdList["DRUID_MACRO_HELPER"] = function(parameters)
+  DruidMacroSlash(strsplit(" ", parameters));
+end;
+
+do
+    local b = _G[DRUID_MACRO_HELPER_NAME_START] or CreateFrame('Button', DRUID_MACRO_HELPER_NAME_START, nil, 'SecureActionButtonTemplate,SecureHandlerBaseTemplate');
+    b:SetAttribute('type', 'macro');
+    b:SetAttribute('macrotext', '/changeactionbar [noform]1;[form:1]2;[form:3]3;[form:4]4;[form:5]5;6;\n/dmh start');
+end
+do
+    local b = _G[DRUID_MACRO_HELPER_NAME_RESET] or CreateFrame('Button', DRUID_MACRO_HELPER_NAME_RESET, nil, 'SecureActionButtonTemplate,SecureHandlerBaseTemplate');
+    b:SetAttribute('type', 'macro');
+    b:SetAttribute('macrotext', '/changeactionbar 1');
+end
+do
+    local b = _G[DRUID_MACRO_HELPER_NAME_END] or CreateFrame('Button', DRUID_MACRO_HELPER_NAME_END, nil, 'SecureActionButtonTemplate,SecureHandlerBaseTemplate');
+    b:SetAttribute('type', 'macro');
+    b:SetAttribute('macrotext', '/use [bar:2]!Dire Bear Form;[bar:3]!Cat Form;[bar:4]!Travel Form\n/click '..DRUID_MACRO_HELPER_NAME_RESET..'\n/dmh end');
+end
+do
+    local b = _G[DRUID_MACRO_HELPER_NAME_POT] or CreateFrame('Button', DRUID_MACRO_HELPER_NAME_POT, nil, 'SecureActionButtonTemplate,SecureHandlerBaseTemplate');
+    b:SetAttribute('type', 'macro');
+    b:SetAttribute('macrotext', '/dmh cd pot\n/dmh start');
+end
+do
+    local b = _G[DRUID_MACRO_HELPER_NAME_HS] or CreateFrame('Button', DRUID_MACRO_HELPER_NAME_HS, nil, 'SecureActionButtonTemplate,SecureHandlerBaseTemplate');
+    b:SetAttribute('type', 'macro');
+    b:SetAttribute('macrotext', '/dmh cd hs\n/dmh start');
+end
+do
+    local b = _G[DRUID_MACRO_HELPER_NAME_SAPPER] or CreateFrame('Button', DRUID_MACRO_HELPER_NAME_SAPPER, nil, 'SecureActionButtonTemplate,SecureHandlerBaseTemplate');
+    b:SetAttribute('type', 'macro');
+    b:SetAttribute('macrotext', '/dmh cd sapper\n/dmh start');
+end
+do
+    local b = _G[DRUID_MACRO_HELPER_NAME_SUPER_SAPPER] or CreateFrame('Button', DRUID_MACRO_HELPER_NAME_SUPER_SAPPER, nil, 'SecureActionButtonTemplate,SecureHandlerBaseTemplate');
+    b:SetAttribute('type', 'macro');
+    b:SetAttribute('macrotext', '/dmh cd supersapper\n/dmh start');
+end
---
+++ README.md
@@ -0,0 +1,101 @@
+# DruidMacroHelper (TBC Classic) by Mylaerla-Everlook
+
+This addon aims to simplify consume/powershifting macros as much as possible,
+making them more readable and massively reduce the character count.
+
+Credit to:
+- Fyroth and Zia for coming up / bringing to attention this improved method of powershifting
+- PsiloShift / Psilocybin-Mograine(H) for providing inspiration to further improve the addon
+- The youtuber griftin for bringing those macro to my attention (see https://www.youtube.com/watch?v=wAjeA4ZokyY )
+
+The addon allows your macros to prevent shifting out of form if:
+- You are stunned/feared/...
+- You are on global cooldown
+- You don't have enough mana to shift back
+And optionally
+- If specific items are on cooldown
+- If your enegery is above a certain threshold
+
+## Download
+
+You can download the latest Version on CurseForge (manually or via the app):
+https://www.curseforge.com/wow/addons/druidmacrohelper
+
+## Examples:
+
+### Cat powershifting
+#### Will shift out of cat form and back in if you are below 30 energy
+```lua
+#showtooltip
+/stopattack
+/dmh energy 30
+/cast !Cat Form
+/dmh end
+/startattack
+```
+
+### Healthstone
+#### Will shift out of form, use a Healthstone and shift back into the form you started in
+```lua
+#showtooltip
+/stopattack
+/click dmhStart
+/dmh cd hs
+/use Master Healthstone
+/click dmhEnd
+/startattack
+```
+
+### Super Healing Potion
+#### Will shift out of form, use a Super Healing Potion and shift back into the form you started in
+```lua
+#showtooltip
+/stopattack
+/click dmhStart
+/dmh cd pot
+/use Super Healing Potion
+/click dmhEnd
+/startattack
+```
+
+### Goblin Sapper
+#### Will shift out of form, use a Super Sapper Charge / Goblin Sapper Charge and shift back into the form you started in
+```lua
+#showtooltip
+/click [mod:shift]dmhSuperSap;dmhSap
+/click dmhStart
+/use [mod:shift]Super Sapper Charge;Goblin Sapper Charge
+/click dmhEnd
+```
+
+## Available commands
+
+There are two options on how to use the addon in macros:
+
+### Click variants
+The click variants allow to use modifiers (like shown in the sapper example)
+* `/click dmhStart` Change actionbar based on the current form. (includes /dmh start)
+* `/click dmhReset` Change actionbar back to 1.
+* `/click dmhEnd` Change back to form based on the current bar. (includes /dmh end)
+* `/click dmhPot` Disable autoUnshift if not ready to use a potion
+* `/click dmhHs` Disable autoUnshift if not ready to use a healthstone
+* `/click dmhSap` Disable autoUnshift if not ready to use a sapper
+* `/click dmhSuperSap` Disable autoUnshift if not ready to use a super sapper
+
+### Slash commands
+The slash commands allow for more flexibility in some cases (like custom itemIds or energy-based shifting)
+* `/dmh start`
+
+    Disable autoUnshift if player is stunned, on gcd or out of mana
+* `/dmh end`
+
+    Enable autoUnshift again
+* `/dmh stun`
+
+    Disable autoUnshift if stunned    
+* `/dmh cd <itemId|itemShortcut>[ <itemId|itemShortcut> ...]`
+
+    Disable autoUnshift if items are on cooldown, player is stunned, on gcd or out of mana
+* `/dmh energy <maxEnergy>`
+
+    Disable autoUnshift if above given energy value, player is stunned, on gcd or out of mana
